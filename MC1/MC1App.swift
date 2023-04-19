8ff905e155912014ba57ed3cd1b3bff9969387f5
Nuki Venoza <nukivenoza03@gmail.com>
2023-04-19 17:05:33 +0700

    initial commit

---
+++ MC1/MC1App.swift
@@ -0,0 +1,17 @@
+//
+//  MC1App.swift
+//  MC1
+//
+//  Created by Nuki Venoza on 19/04/23.
+//
+
+import SwiftUI
+
+@main
+struct MC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
