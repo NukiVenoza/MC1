d3b674a215775a5265ca0ad3e8cc6bd0d4702606
Devin <devin.maleke@gmail.com>
2023-04-29 18:03:55 +0700

    play and pause exercise + animations

--- MC1/ExerciseDetailView.swift
+++ MC1/ExerciseDetailView.swift
@@ -17,34 +17,46 @@ struct ExerciseDetailView: View {
     var body: some View {
         NavigationView{
             VStack (alignment: .leading){
-                // Image
-                //            Image(uiImage: background)
-                Text(exercise.name)
-                    .foregroundColor(.black)
-                    .fontWeight(.semibold)
-                    .font(.system(size: 20))
-                    .padding()
-                
-                HStack {
-                    Image(systemName: "timer")
+                VStack{
+                    Image(exercise.bg)
                         .resizable()
-                        .frame(width: 25, height: 25)
-                        .foregroundColor(Color(red: 0.066, green: 0.463, blue: 0.415))
-                    
-                    Text(exercise.duration)
-                        .fontWeight(.regular)
-                        .font(.system(size: 17))
-                        .foregroundColor(Color(red: 0.3333333333333333, green: 0.3333333333333333, blue: 0.3333333333333333))
-                        
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .edgesIgnoringSafeArea(.all)
                 }
-                .padding()
                 
-                Text(exercise.desc)
-                    .fontWeight(.regular)
-                    .frame(width: 326, height: 72)
-                    .font(.system(size: 15))
+                VStack(alignment: .leading){
+                    Text(exercise.name)
+                        .foregroundColor(.black)
+                        .fontWeight(.semibold)
+                        .font(.system(size: 20))
+                        .padding(.bottom,15)
+                        
+                    
+                    HStack {
+                        Image(systemName: "timer")
+                            .resizable()
+                            .frame(width: 25, height: 25)
+                            .foregroundColor(Color(red: 0.066, green: 0.463, blue: 0.415))
+                        
+                        Text(exercise.duration)
+                            .fontWeight(.regular)
+                            .font(.system(size: 17))
+                            .foregroundColor(Color(red: 0.3333333333333333, green: 0.3333333333333333, blue: 0.3333333333333333))
+                        
+                    }
+                    .padding(.bottom,15)
+                    
+                    
+                    Text(exercise.desc)
+                        .fontWeight(.regular)
+                        .frame(width: 326, height: 72)
+                        .font(.system(size: 15))
+                        
+                        
+                    
+                }.padding(.leading,20)
                     .padding()
-                
+                    
                 
 //                NavigationLink(destination: ExerciseRequirementView()){
                 Text("Start")
@@ -57,16 +69,17 @@ struct ExerciseDetailView: View {
                     .cornerRadius(10)
 //                }
                 
-                .padding()
+                    .padding(.bottom,20)
+                    .padding(30)
+                
             }
-            .padding()
+
         }
     }
 }
 
-
 struct ExerciseDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ExerciseDetailView(exercise: ExerciseModel(id: 1, name: "3 Minutes Breath", duration: "3 Minutes", desc: "Using mindfulness of breathing for short periods at set times and when required. Best for dealing with mind wandering, and learning to be gentle with yourself.", requirement: ["Airpods or a conducive place", "A safe place for sit or lay down", "Turn on focus mode or do not disturb"], icon: "icon3MB", backgroundURL: "Cards/3MB"))
+        ExerciseDetailView(exercise: ExerciseModel(id: 1, name: "3 Minutes Breath", duration: "3 Minutes", desc: "Using mindfulness of breathing for short periods at set times and when required. Best for dealing with mind wandering, and learning to be gentle with yourself.", requirement: ["Airpods or a conducive place", "A safe place for sit or lay down", "Turn on focus mode or do not disturb"], icon: "icon3MB", backgroundURL: "Cards/3MB", bg: "bg3MB"))
     }
 }
--- MC1/ExercisePlayerView.swift
+++ MC1/ExercisePlayerView.swift
@@ -70,6 +70,6 @@ struct ExercisePlayerView: View {
 
 struct ExercisePlayerView_Previews: PreviewProvider {
     static var previews: some View {
-        ExercisePlayerView(exercise: ExerciseModel(id: 1, name: "3 Minutes Breath", duration: "3 Minutes", desc: "Using mindfulness of breathing for short periods at set times and when required. Best for dealing with mind wandering, and learning to be gentle with yourself.", requirement: ["Airpods or a conducive place", "A safe place for sit or lay down", "Turn on focus mode or do not disturb"], icon: "icon3MB", backgroundURL: "Cards/3MB"))
+        ExercisePlayerView(exercise: ExerciseModel(id: 1, name: "3 Minutes Breath", duration: "3 Minutes", desc: "Using mindfulness of breathing for short periods at set times and when required. Best for dealing with mind wandering, and learning to be gentle with yourself.", requirement: ["Airpods or a conducive place", "A safe place for sit or lay down", "Turn on focus mode or do not disturb"], icon: "icon3MB", backgroundURL: "Cards/3MB", bg: "bg3MB"))
     }
 }
